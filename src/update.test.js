02287cc79ea984beee16a885f98f1bc814188871
Raga <ragawilliam570@gmail.com>
2022-08-18 12:24:08 +0300

    Wrote tests for edit, change status and clear all complete tasks functions

---
+++ src/update.test.js
@@ -0,0 +1,16 @@
+import { checkIfChecked, clearCompleteTasks, editTask } from './modules/editutils.js';
+import localStorageMock from './__mocks__/localStorage.js';
+
+test('Edit task', () => {
+  expect(editTask(localStorageMock, 0, 'Edit')).toEqual(localStorageMock[0]);
+  expect(editTask(localStorageMock, 1, 'Chess')).toEqual(localStorageMock[1]);
+});
+
+test('Change status', () => {
+  expect(checkIfChecked(localStorageMock, 0)).toEqual(localStorageMock[0].isComplete);
+  expect(checkIfChecked(localStorageMock, 4)).toEqual(localStorageMock[4].isComplete);
+});
+
+test('Clear all completed', () => {
+  expect(clearCompleteTasks(localStorageMock).length).toBe(1);
+});
